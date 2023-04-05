c708c20075798a3266b4b90eb74e21864511cd8e
joselinecedano <118120312+joselinecedano@users.noreply.github.com>
2023-04-04 21:26:50 -0400

    changed index to server.js in package, project and about routes working

--- server.js
+++ server.js
@@ -3,15 +3,26 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+//IMPORT JSON FILES
+const projects = require('./projects.json');
+const about = require('./about.json');
 
 //MIDDLEWARE
 app.use(cors());
 
 //ROUTES
+    //home route
 app.get('/', (req,res)=>{
     res.send('HW 20 Express React App!!')
-})
-
+});
+    //projects route
+app.get('/projects', (req,res)=>{
+    res.json(projects) //<--retrieves and sends projects 
+});
+    //about route
+app.get('/about', (req,res)=>{
+    res.json(about) //<--retrieves and sends about  
+});
 
 //LISTENER
 const PORT = process.env.PORT || 4000;
